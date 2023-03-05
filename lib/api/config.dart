a849a41bb0a53625379f7504a947e23e082390b3
zelalem <zelalemgizachew890@gmail.com>
2023-03-05 15:53:59 +0300

    Authentication & Authorization Completed! Testing is not done yet.

--- lib/api/config.dart
+++ lib/api/config.dart
@@ -1,5 +1,9 @@
 class ApiConstants {
-  static String BASE_URL = 'http://127.0.0.1:8000';
-  static String USER_LOGIN = '/auth/jwt/create';
-  static String USER_REGISTER = '/auth/users';
+  // static String BASE_URL = 'http://127.0.0.1:8000';
+  static String BASE_URL = 'https://70fc-196-189-24-48.eu.ngrok.io';
+  static String USER_LOGIN = '/auth/jwt/create/';
+  static String USER_REGISTER = '/auth/users/';
+  static String USER_ACCOUNT = '/auth/users/me/';
+  static String RESTAURANT_REGISTER = '/api/v1/fastrack/restaurant/';
+  static String USER_PROFILE = '/api/v1/fastrack/restaurant/me/';
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:localstorage/localstorage.dart';
+import 'package:restaurantapp/api/config.dart';
+import 'package:restaurantapp/screens/continue_register_screen.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  isIExist() async {
+    // print("THIS IS RUNNGING:");
+    try {
+      String access_token = LocalStorage('tokens').getItem('access');
+      var url = Uri.parse(ApiConstants.BASE_URL + ApiConstants.USER_PROFILE);
+      var response = await http
+          .get(url, headers: {"Authorization": "JWT " + access_token});
+      if (response.statusCode == 404) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ContinueREgister(),
+          ),
+        );
+      } else {
+        // There is already a profile with this account
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    isIExist();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
